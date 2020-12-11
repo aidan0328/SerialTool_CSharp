7fc762b7a59e97c02be478db33a2f69a45895dce
aidan.lu <aidan0328@gmail.com>
2020-12-12 00:42:08 +0800

    導入 Thread()，可以在傳送的時候，輸入 ENTER 來結束程序。

--- SerialTool_CSharp/Program.cs
+++ SerialTool_CSharp/Program.cs
@@ -1,25 +1,30 @@
 ﻿using System;
 using System.IO.Ports;
-using System.Globalization;
+using System.Threading;
 
 namespace SerialTool_CSharp {
     public class Program {
-        public static void Main() {
-
+        public static void transmitMsg() {
             SerialPort serialPort = new SerialPort("COM3", 115200);
             serialPort.Open();
             Byte[] packet = new Byte[] { 0x30, 0x31, 0x32, 0x33, 0x34 };
             DateTime dt1 = new DateTime();
+            Console.Write("Transmitting");
             while (true) {
                 if (new TimeSpan(DateTime.Now.Ticks - dt1.Ticks).TotalMilliseconds > 500) {
                     dt1 = DateTime.Now;
                     serialPort.Write(packet, 0, packet.Length);
+                    Console.Write(".");
                 }
             }
-            serialPort.Close();
+        }
+        public static void Main() {
+            var t = new Thread(transmitMsg);
+            t.Start();
 
             Console.WriteLine("Press ENTER to exit.");
             Console.ReadLine();
+            t.Abort();
         }
     }
 }
