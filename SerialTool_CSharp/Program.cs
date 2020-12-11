3f6b949d07a1bf22614f451a9f6a9dbae7be5878
aidan.lu <aidan0328@gmail.com>
2020-12-12 00:05:45 +0800

    每 500ms 傳送一筆

--- SerialTool_CSharp/Program.cs
+++ SerialTool_CSharp/Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.IO.Ports;
+using System.Globalization;
 
 namespace SerialTool_CSharp {
     public class Program {
         public static void Main() {
+
             SerialPort serialPort = new SerialPort("COM3", 115200);
             serialPort.Open();
             Byte[] packet = new Byte[] { 0x30, 0x31, 0x32, 0x33, 0x34 };
-            serialPort.Write(packet, 0, packet.Length);
+            DateTime dt1 = new DateTime();
+            while (true) {
+                if (new TimeSpan(DateTime.Now.Ticks - dt1.Ticks).TotalMilliseconds > 500) {
+                    dt1 = DateTime.Now;
+                    serialPort.Write(packet, 0, packet.Length);
+                }
+            }
             serialPort.Close();
 
             Console.WriteLine("Press ENTER to exit.");
