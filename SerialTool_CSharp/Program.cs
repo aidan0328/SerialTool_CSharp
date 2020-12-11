6aca808eac4ab4f505fe9019b38acb70271dbb79
aidan.lu <aidan0328@gmail.com>
2020-12-11 23:24:49 +0800

    加入 NUint。

--- SerialTool_CSharp/Program.cs
+++ SerialTool_CSharp/Program.cs
@@ -2,17 +2,16 @@
 using System.IO.Ports;
 
 namespace SerialTool_CSharp {
-    class Program {
-        SerialPort serialPort;
-        static void Main(string[] args) {
+    public class Program {
+        public static void Main() {
             string[] ports = SerialPort.GetPortNames();
 
             foreach (string p in ports) {
                 Console.WriteLine(p);
             }
 
-            Console.WriteLine("Press ENTER to exit.");
-            Console.ReadLine();
+            //Console.WriteLine("Press ENTER to exit.");
+            //Console.ReadLine();
         }
     }
 }
---
+++ SerialTool_CSharpTests/ProgramTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using SerialTool_CSharp;
+
+namespace SerialTool_CSharp.Tests {
+    [TestFixture()]
+    public class ProgramTests {
+        [Test()]
+        public void MainTest() {
+            Program.Main();
+        }
+    }
+}
