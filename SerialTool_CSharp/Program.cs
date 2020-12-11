f957b6b92699819d289f5ea3122e0f82153eb453
aidan.lu <aidan0328@gmail.com>
2020-12-12 00:47:01 +0800

    使用可以帶入參數的 Thread。

--- SerialTool_CSharp/Program.cs
+++ SerialTool_CSharp/Program.cs
@@ -18,9 +18,25 @@ namespace SerialTool_CSharp {
                 }
             }
         }
+
+        public static void transmitWithMsg(Object msg) {
+            SerialPort serialPort = new SerialPort("COM3", 115200);
+            serialPort.Open();
+            Byte[] packet = (Byte[])msg;
+            DateTime dt1 = new DateTime();
+            Console.Write("Transmitting");
+            while (true) {
+                if (new TimeSpan(DateTime.Now.Ticks - dt1.Ticks).TotalMilliseconds > 500) {
+                    dt1 = DateTime.Now;
+                    serialPort.Write(packet, 0, packet.Length);
+                    Console.Write(".");
+                }
+            }
+        }
         public static void Main() {
-            var t = new Thread(transmitMsg);
-            t.Start();
+            var t = new Thread(transmitWithMsg);
+            Byte[] packet = new Byte[] { 0x65, 0x66, 0x67, 0x68, 0x69 };
+            t.Start(packet);
 
             Console.WriteLine("Press ENTER to exit.");
             Console.ReadLine();
