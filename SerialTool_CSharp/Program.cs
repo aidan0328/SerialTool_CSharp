def2b983d6d7f5fcc8be0dc5f992bf08afe484a8
aidan.lu <aidan0328@gmail.com>
2020-12-11 23:49:42 +0800

    傳送 Byte[]

--- SerialTool_CSharp/Program.cs
+++ SerialTool_CSharp/Program.cs
@@ -6,7 +6,8 @@ namespace SerialTool_CSharp {
         public static void Main() {
             SerialPort serialPort = new SerialPort("COM3", 115200);
             serialPort.Open();
-            serialPort.WriteLine($"From {serialPort.PortName}, Hello");
+            Byte[] packet = new Byte[] { 0x30, 0x31, 0x32, 0x33, 0x34 };
+            serialPort.Write(packet, 0, packet.Length);
             serialPort.Close();
 
             Console.WriteLine("Press ENTER to exit.");
