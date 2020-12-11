3d54cff82c25cb3b629a45c1d9f451db91ac1a1d
aidan.lu <aidan0328@gmail.com>
2020-12-11 23:43:00 +0800

    開啟,傳送並立即關閉

--- SerialTool_CSharp/Program.cs
+++ SerialTool_CSharp/Program.cs
@@ -4,14 +4,13 @@ using System.IO.Ports;
 namespace SerialTool_CSharp {
     public class Program {
         public static void Main() {
-            string[] ports = SerialPort.GetPortNames();
+            SerialPort serialPort = new SerialPort("COM3", 115200);
+            serialPort.Open();
+            serialPort.WriteLine($"From {serialPort.PortName}, Hello");
+            serialPort.Close();
 
-            foreach (string p in ports) {
-                Console.WriteLine(p);
-            }
-
-            //Console.WriteLine("Press ENTER to exit.");
-            //Console.ReadLine();
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine();
         }
     }
 }
