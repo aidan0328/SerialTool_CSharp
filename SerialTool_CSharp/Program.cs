efc2b501cbd502688cd0bc506b1fee95a7c0df96
aidan.lu <aidan0328@gmail.com>
2020-12-12 21:01:15 +0800

    將收到的封包立即回傳回來 (Echo Test)

--- SerialTool_CSharp/Program.cs
+++ SerialTool_CSharp/Program.cs
@@ -5,6 +5,10 @@ using System.Timers;
 namespace SerialTool_CSharp {
     public class Program {
         static SerialPort serialPort;
+        static Byte[] packet;
+        static int packetNumber;
+        static int readBytes;
+
         /// <summary>
         /// serialPort 收到資料時的 Event Handle
         /// </summary>
@@ -12,14 +16,9 @@ namespace SerialTool_CSharp {
         /// <param name="e"></param>
         private static void DataReceivedEventHandler(object sender, SerialDataReceivedEventArgs e) {
             SerialPort serialPort = (SerialPort)sender;
-            Byte[] buffer = new Byte[serialPort.BytesToRead];
-            int readBytes = serialPort.Read(buffer, 0, buffer.Length);
-
-            Console.WriteLine($"從 {serialPort.PortName} 收到");
-            for (int i = 0; i < readBytes; i++) {
-                Console.Write(String.Format("0x{0:X} ,", buffer[i]));
-            }
-            Console.WriteLine();
+            packet = new Byte[serialPort.BytesToRead];
+            packetNumber++;
+            readBytes += serialPort.Read(packet, 0, packet.Length);
         }
 
         /// <summary>
@@ -28,9 +27,12 @@ namespace SerialTool_CSharp {
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private static void OnTimedEvent(Object source, ElapsedEventArgs e) {
-            if (serialPort.IsOpen) {
-                Byte[] packet = new Byte[] { 0x30, 0x31, 0x32, 0x33, 0x34 };
-                serialPort.Write(packet, 0, packet.Length);
+            if ((serialPort.IsOpen)
+             && (readBytes > 0)) {
+                Console.WriteLine($"從 {serialPort.PortName} 收到 {packetNumber} 筆封包，長度為 {readBytes} bytes");
+                serialPort.Write(packet, 0, readBytes);
+                packetNumber = 0;
+                readBytes = 0;
             }
         }
 
