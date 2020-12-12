fb35b20207745cd6df1d033b02d815f5ea925ec9
aidan.lu <aidan0328@gmail.com>
2020-12-12 19:56:39 +0800

    使用 DataReceived event 讀取 Byte 陣列

--- SerialTool_CSharp/Program.cs
+++ SerialTool_CSharp/Program.cs
@@ -3,21 +3,25 @@ using System.IO.Ports;
 
 namespace SerialTool_CSharp {
     public class Program {
-        public static void Main() {
-            SerialPort serialPort = new SerialPort("COM3", 115200);
-            serialPort.Open();
-            Byte[] buffer = new Byte[10];
+        public static void DataReceivedEventHandler(object sender, SerialDataReceivedEventArgs e) {
+            SerialPort serialPort = (SerialPort)sender;
+            Byte[] buffer = new Byte[serialPort.BytesToRead];
             int readBytes = serialPort.Read(buffer, 0, buffer.Length);
-            serialPort.Close();
 
             Console.WriteLine($"從 {serialPort.PortName} 收到");
             for (int i = 0; i < readBytes; i++) {
                 Console.Write(String.Format("0x{0:X} ,", buffer[i]));
             }
             Console.WriteLine();
+        }
+        public static void Main() {
+            SerialPort serialPort = new SerialPort("COM3", 115200);
+            serialPort.DataReceived += DataReceivedEventHandler;
+            serialPort.Open();
 
             Console.WriteLine("Press ENTER to exit.");
             Console.ReadLine();
+            serialPort.Close();
         }
     }
 }
